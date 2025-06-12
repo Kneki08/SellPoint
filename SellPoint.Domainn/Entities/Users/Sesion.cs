14d3d43b5d5e388e27de88ac1f76bd30b183c2ea
JeanCarlos-30 <jeancarlosterrero30@gmail.com>
2025-06-12 17:07:17 -0400

    Terminacion de la class sesion

--- SellPoint.Domainn/Entities/Users/Sesion.cs
+++ SellPoint.Domainn/Entities/Users/Sesion.cs
@@ -4,8 +4,16 @@ namespace SellPoint.Domain.Entities.Users
 {
     public sealed class Sesion : AuditEntity
     {
-      public int UsuarioId { get; set; }
-      public DateTime FechaInicioSesion { get; set; }
-      public DateTime? FechaCierreSesion { get; set; }
+        public int UsuarioId { get; set; }
+
+        public string TokenSesion { get; set; } = null!;
+
+        public string? DireccionIP { get; set; }
+
+        public string? UserAgent { get; set; }
+
+        public DateTime FechaExpiracion { get; set; }
+
+        public bool Activa { get; set; } = true;
     }
 }
