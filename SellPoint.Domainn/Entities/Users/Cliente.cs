9b7665dd15fc779d8a91fc640268a980d6322f7c
mariaLamarche <mariadelosangeleslamarche@gmail.com>
2025-06-12 06:41:08 -0400

    Update Cliente.cs

--- SellPoint.Domainn/Entities/Users/Cliente.cs
+++ SellPoint.Domainn/Entities/Users/Cliente.cs
@@ -6,6 +6,8 @@ namespace SellPoint.Domainn.Entities.Users
 {
     public sealed class Cliente : Usuario
     {
-
+        public string? DireccionEnvio { get; set; }
+        public decimal SaldoCredito { get; set; }
+        public DateTime FechaRegistro { get; set; } = DateTime.UtcNow;
     }
 }
