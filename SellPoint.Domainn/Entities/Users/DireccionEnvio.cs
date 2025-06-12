67cb9e937800a8c965cf0ea65b458a54242f90a1
JeanCarlos-30 <jeancarlosterrero30@gmail.com>
2025-06-12 16:33:50 -0400

    arregle la class DireccionEnvio

--- SellPoint.Domainn/Entities/Users/DireccionEnvio.cs
+++ SellPoint.Domainn/Entities/Users/DireccionEnvio.cs
@@ -4,7 +4,20 @@ namespace SellPoint.Domain.Entities.Users
 {
     public sealed class DireccionEnvio : AuditEntity
     {
-       public string? Calle { get; set; }
-       public string? Ciudad { get; set; }
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public string Usuario { get; set; }
+
+        public string NombreCompleto { get; set; }
+        public string DireccionLinea1 { get; set; }
+        public string DireccionLinea2 { get; set; }
+        public string Ciudad { get; set; }
+        public string EstadoProvincia { get; set; }
+        public string CodigoPostal { get; set; }
+        public string Pais { get; set; } = "República Dominicana";
+        public string Telefono { get; set; }
+
+        public bool EsPrincipal { get; set; }
+        public bool Activo { get; set; } = true;
     }
 }
