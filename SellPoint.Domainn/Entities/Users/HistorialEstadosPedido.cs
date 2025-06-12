c5ea3863cc5ac01e1c0e8c7e089057631634f2eb
JeanCarlos-30 <jeancarlosterrero30@gmail.com>
2025-06-12 16:54:34 -0400

    Realizacion de la class HistorialEstadosPedido

--- SellPoint.Domainn/Entities/Users/HistorialEstadosPedido.cs
+++ SellPoint.Domainn/Entities/Users/HistorialEstadosPedido.cs
@@ -1,7 +1,19 @@
 ﻿
+using SellPoint.Domainn.Base;
+using SellPoint.Domainn.Entities.Orders;
+
 namespace SellPoint.Domain.Entities.Users
 {
-    public sealed class HistorialEstadosPedido
+    public sealed class HistorialEstadosPedido : AuditEntity
     {
+        public int PedidoId { get; set; }
+        public string? EstadoAnterior { get; set; }
+        public string EstadoNuevo { get; set; } = null!;
+        public string? Comentario { get; set; }
+        public int? UsuarioAdminId { get; set; }
+
+        public Pedido? Pedido { get; set; }
+        public UsuarioBase? UsuarioAdmin { get; set; }
+
     }
 }
