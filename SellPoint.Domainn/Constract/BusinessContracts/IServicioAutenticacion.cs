f9c0c8a9de1c4ad2faa7f292d97122e2329bf1fe
REYNALDO <REYNALDO@LAPTOP-8D7IK6GA>
2025-06-12 18:15:31 -0400

    carpeta d contraos

---
+++ SellPoint.Domainn/Constract/BusinessContracts/IServicioAutenticacion.cs
@@ -0,0 +1,18 @@
+﻿using SellPoint.Domainn.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SellPoint.Domain.Constract.BusinessContracts
+{
+    public interface IServicioAutenticacion
+    {
+        Task<(bool Exitoso, Usuario Usuario, string Mensaje)> IniciarSesionAsync(string email, string password);
+        Task<(bool Exitoso, string Mensaje)> RegistrarUsuarioAsync(RegistroUsuarioDto registroDto);
+        Task<bool> CerrarSesionAsync(int usuarioId);
+        Task<string> GenerarHashPasswordAsync(string password);
+        Task<bool> ValidarPasswordAsync(string password, string hash);
+    }
+}
