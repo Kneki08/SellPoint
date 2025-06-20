d02144a992c561d0e7ccdf968e69988b870608ba
REYNALDO <REYNALDO@LAPTOP-8D7IK6GA>
2025-06-20 16:35:59 -0400

    agregando el operationResult

---
+++ SellPoint.Domainn/Base/OperationResult.cs
@@ -0,0 +1,17 @@
+﻿
+namespace SellPoint.Domain.Base
+{
+
+    public class OperationResult
+    {
+        public bool IsSucess { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public dynamic? Data { get; set; }
+
+        public OperationResult()
+        {
+            this.IsSucess = true;
+        }
+
+    }
+}
