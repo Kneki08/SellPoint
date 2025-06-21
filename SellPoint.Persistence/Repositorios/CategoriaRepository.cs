00f162fa602b71e2d2028c173d5aef44f88c9814
mariaLamarche <mariadelosangeleslamarche@gmail.com>
2025-06-21 17:32:31 -0400

    Update CategoriaRepository.cs

--- SellPoint.Persistence/Repositorios/CategoriaRepository.cs
+++ SellPoint.Persistence/Repositorios/CategoriaRepository.cs
@@ -2,6 +2,6 @@ namespace SellPoint.Persistence.Repositorios
 {
     public class CategoriaRepository : GenericRepository<Categoria>, ICategoriaRepository
     {
-       public CategoriaRepository(string connectionString) : base(context) { }
+       public CategoriaRepository(string connectionString) : base(connectionString) { }
     }
 }
