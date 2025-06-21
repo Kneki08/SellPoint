82066756ef3b22d7b19c9d360125e001b08b5527
mariaLamarche <mariadelosangeleslamarche@gmail.com>
2025-06-21 15:35:00 -0400

    Update ICarritoRepository.cs

--- SellPoint.Aplication/Interfaces/Repositorios/ICarritoRepository.cs
+++ SellPoint.Aplication/Interfaces/Repositorios/ICarritoRepository.cs
@@ -3,8 +3,8 @@ namespace SellPoint.Aplication.Interfaces.Repositorios
 {
     public interface ICarritoRepository
     {
-        Task<OperationResult> AddProductAsync(int productoId, int cantidad);
-        Task<OperationResult> DeleteProductAsync(int productoId);
+        Task<OperationResult> AddProductoAsync(int productoId, int cantidad);
+        Task<OperationResult> DeleteProductoAsync(int productoId);
         Task<OperationResult> CleanCarritoAsync();
         Task<OperationResult<List<CarritoItem>>> GetItemsAsync();
         Task<OperationResult<decimal>> GetTotalAsync();
