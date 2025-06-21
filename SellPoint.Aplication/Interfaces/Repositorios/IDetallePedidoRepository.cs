b05b62dc5fee4a87854ba47b68526b7acd104e84
mariaLamarche <mariadelosangeleslamarche@gmail.com>
2025-06-21 15:26:00 -0400

    Update IDetallePedidoRepository.cs

--- SellPoint.Aplication/Interfaces/Repositorios/IDetallePedidoRepository.cs
+++ SellPoint.Aplication/Interfaces/Repositorios/IDetallePedidoRepository.cs
@@ -2,6 +2,10 @@ namespace SellPoint.Aplication.Interfaces.Repositorios
 {
     public interface IDetallePedidoRepository
     {
-    
+        Task<OperationResult> CreateDetalleAsync(DetallePedido detalle);
+        Task<OperationResult> UpdateDetalleAsync(DetallePedido detalle);
+        Task<OperationResult> DeleteDetalleAsync(int detalleId);
+        Task<OperationResult<DetallePedido>> GetDetalleByIdAsync(int detalleId);
+        Task<OperationResult<List<DetallePedido>>> GetDetallesByPedidoIdAsync(int pedidoId);
     }
 }
