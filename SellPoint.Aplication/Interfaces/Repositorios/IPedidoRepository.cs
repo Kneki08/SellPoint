b0b8d51784f064e4baae4b756394f6a13b21cc5d
mariaLamarche <mariadelosangeleslamarche@gmail.com>
2025-06-21 15:29:20 -0400

    Update IPedidoRepository.cs

--- SellPoint.Aplication/Interfaces/Repositorios/IPedidoRepository.cs
+++ SellPoint.Aplication/Interfaces/Repositorios/IPedidoRepository.cs
@@ -2,6 +2,12 @@ namespace SellPoint.Aplication.Interfaces.Repositorios
 {
     public interface IPedidoRepository
     {
-    
+        Task<OperationResult> CreatePedidoAsync(Pedido pedido);
+        Task<OperationResult> UpdatePedidoAsync(Pedido pedido);
+        Task<OperationResult> DeletePedidoAsync(int pedidoId);
+        Task<OperationResult<Pedido>> GetPedidoByIdAsync(int pedidoId);
+        Task<OperationResult<List<Pedido>>> GetAllPedidosAsync();
+        Task<OperationResult<List<Pedido>>> GetPedidosByClienteIdAsync(int clienteId);
+        Task<OperationResult> ChangeStatusPedidoAsync(int pedidoId, string nuevoEstado);
     }
 }
