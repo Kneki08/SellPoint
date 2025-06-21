407cc7d600c48754894cbba24e7d24e8f9d78b0a
REYNALDO <REYNALDO@LAPTOP-8D7IK6GA>
2025-06-21 10:09:03 -0400

    ...

--- SellPoint.Aplication/Dtos/Producto/SaveProductoDTO.cs
+++ SellPoint.Aplication/Dtos/Producto/SaveProductoDTO.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SellPoint.Aplication.Dtos.Producto
 {
-    public class SaveProductoDTO : ProductoDTO
+    public class SaveProductoDTO 
     {
      
     }
