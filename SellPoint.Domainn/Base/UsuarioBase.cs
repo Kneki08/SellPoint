f96efdac1ecef8a02c5a9375fab7efbc40b02671
REYNALDO <REYNALDO@LAPTOP-8D7IK6GA>
2025-06-21 19:26:07 -0400

    ...

--- SellPoint.Domainn/Base/UsuarioBase.cs
+++ SellPoint.Domainn/Base/UsuarioBase.cs
@@ -9,7 +9,7 @@ namespace SellPoint.Domainn.Base
      public string? Apellido {  get; set; }
      public string? FullName => $"{Nombre} {Apellido}";
      public string? Email {  get; set; }
-     public string Telefono {  get; set; }
+     public string? Telefono {  get; set; }
      public DateTime FechaDeNacimiento { get; set; }
     }
 }
--- SellPoint.Domainn/Entities/Users/DireccionEnvio.cs
+++ SellPoint.Domainn/Entities/Users/DireccionEnvio.cs
@@ -6,16 +6,16 @@ namespace SellPoint.Domain.Entities.Users
     {
         public int Id { get; set; }
         public int UsuarioId { get; set; }
-        public string Usuario { get; set; }
+        public string? Usuario { get; set; }
 
-        public string NombreCompleto { get; set; }
-        public string DireccionLinea1 { get; set; }
-        public string DireccionLinea2 { get; set; }
-        public string Ciudad { get; set; }
-        public string EstadoProvincia { get; set; }
-        public string CodigoPostal { get; set; }
+        public string? NombreCompleto { get; set; }
+        public string? DireccionLinea1 { get; set; }
+        public string? DireccionLinea2 { get; set; }
+        public string? Ciudad { get; set; }
+        public string? EstadoProvincia { get; set; }
+        public string? CodigoPostal { get; set; }
         public string Pais { get; set; } = "República Dominicana";
-        public string Telefono { get; set; }
+        public string? Telefono { get; set; }
 
         public bool EsPrincipal { get; set; }
         public bool Activo { get; set; } = true;
