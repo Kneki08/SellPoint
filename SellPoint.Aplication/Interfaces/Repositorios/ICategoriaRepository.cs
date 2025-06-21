d5114bd5c0cb25a0082f7c4e5318c773afa4df0a
mariaLamarche <mariadelosangeleslamarche@gmail.com>
2025-06-21 17:16:20 -0400

    Update ICategoriaRepository.cs

--- SellPoint.Aplication/Interfaces/Repositorios/ICategoriaRepository.cs
+++ SellPoint.Aplication/Interfaces/Repositorios/ICategoriaRepository.cs
@@ -1,11 +1,7 @@
 namespace SellPoint.Aplication.Interfaces.Repositorios
 {
-    public interface ICategoriaRepository
+    public interface ICategoriaRepository : IGenericRepository<Categoria>
     {
-        Task<OperationResult> CreateCategoriaAsync(Catergoria categoria);
-        Task<OperationResult> UpdateCategoriaAsync(Catergoria categoria);
-        Task<OperationResult> DeleteCategoriaAsync(int categoriaId);
-        Task<OperationResult<Categoria>> GetCategoriaByIdAsync(int categoriaId);
-        Task<OperationResult<List<Categoria>>> GetAllCategoriaAsync();
+       
     }
 }
