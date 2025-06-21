84ebc848cf2babfa4fb1eb0d11a6bed07f6a9480
mariaLamarche <mariadelosangeleslamarche@gmail.com>
2025-06-21 17:17:09 -0400

    Update ICuponRepository.cs

--- SellPoint.Aplication/Interfaces/Repositorios/ICuponRepository.cs
+++ SellPoint.Aplication/Interfaces/Repositorios/ICuponRepository.cs
@@ -1,12 +1,7 @@
 namespace SellPoint.Aplication.Interfaces.Repositorios
 {
-    public interface ICuponRepository
+    public interface ICuponRepository : IGenericRepository<Cupon>
     {
-        Task<OperationResult> CreateCuponAsync(Cupon cupon);
-        Task<OperationResult> UpdateCuponAsync(Cupon cupon);
-        Task<OperationResult> DeleteCuponAsync(int cuponId);
-        Task<OperationResult<Cupon>> GetCuponByIdAsync(int cuponId);
-        Task<OperationResult<List<Cupon>>> GetAllCuponAsync();
         Task<OperationResult<Cupon>> ValidateCodigoCuponAsync(string codigo);
     }
 }
