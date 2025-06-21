c5ccac42263b7d43a792a5dd3106c5da41524398
mariaLamarche <mariadelosangeleslamarche@gmail.com>
2025-06-21 17:09:04 -0400

    Update IProductoRepository.cs

--- SellPoint.Aplication/Interfaces/Repositorios/IProductoRepository.cs
+++ SellPoint.Aplication/Interfaces/Repositorios/IProductoRepository.cs
@@ -2,13 +2,8 @@ namespace SellPoint.Aplication.Interfaces.Repositorios
 {
     public interface IProductoRepository
     {
-        Task<OperationResult> CreateProductoAsync(Producto producto);
-        Task<OperationResult> UpdateProductoAsync(Producto producto);
-        Task<OperationResult> DeleteProductoAsync(int productoId);
-        Task<OperationResult<Producto>> GetProductoByIdAsync(int productoId);
-        Task<OperationResult<List<Producto>>> GetAllProductosAsync();
-        Task<OperationResult<List<Producto>>> GetByCategoriaAsync(int categoriaId);
-        Task<OperationResult<List<Producto>>> SearchByNombreAsync(string nombre);
-        Task<OperationResult> ChangeStatusAsync(int productoId, bool activo);
+    Task<OperationResult<List<Producto>>> GetByCategoriaAsync(int categoriaId);
+    Task<OperationResult<List<Producto>>> SearchByNombreAsync(string nombre);
+    Task<OperationResult> ChangeStatusAsync(int productoId, bool activo);
     }
 }
