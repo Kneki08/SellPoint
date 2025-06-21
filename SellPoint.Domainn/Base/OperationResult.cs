43348bfaba031525af3fa72b353236c7d4b29075
REYNALDO <REYNALDO@LAPTOP-8D7IK6GA>
2025-06-21 17:48:28 -0400

    actualizacion en el operationResult

--- SellPoint.Domainn/Base/OperationResult.cs
+++ SellPoint.Domainn/Base/OperationResult.cs
@@ -7,6 +7,27 @@ namespace SellPoint.Domain.Base
         public bool IsSuccess { get; set; }
         public string Message { get; set; } = string.Empty;
         public dynamic? Data { get; set; }
-        public OperationResult() { }
+
+        // Constructores estáticos para claridad al retornar
+        public static OperationResult Success(dynamic? data = null, string message = "Operación exitosa.")
+        {
+            return new OperationResult
+            {
+                IsSuccess = true,
+                Message = message,
+                Data = data
+            };
+        }
+
+        public static OperationResult Failure(string message = "Ocurrió un error.")
+        {
+            return new OperationResult
+            {
+                IsSuccess = false,
+                Message = message
+            };
+        }
+
+
     }
 }
