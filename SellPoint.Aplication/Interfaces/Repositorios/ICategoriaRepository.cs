badc9b8b27f4471f6c18a574ebef891053025e15
REYNALDO <REYNALDO@LAPTOP-8D7IK6GA>
2025-06-21 22:16:06 -0400

    ...

--- SellPoint.Aplication/Interfaces/Repositorios/ICategoriaRepository.cs
+++ SellPoint.Aplication/Interfaces/Repositorios/ICategoriaRepository.cs
@@ -1,13 +1,17 @@
+using SellPoint.Aplication.Dtos.Carrito;
 using SellPoint.Aplication.Interfaces.Base;
 using SellPoint.Domain.Base;
 using SellPoint.Domainn.Entities.Products;
 
 namespace SellPoint.Aplication.Interfaces.Repositorios
 {
-    public interface ICategoriaRepository : IRepository<Categoria>
+    public interface ICategoriaRepository 
     {
-        Task<OperationResult> ObtenerPorNombreAsync(string nombre);
-        Task<OperationResult> ObtenerConProductosAsync(int categoriaId);
+        Task<OperationResult> ObtenerPorIdAsync(int id);
+        Task<OperationResult> ObtenerTodosAsync();
+        Task<OperationResult> AgregarAsync(SaveCarritoDTO entidad);
+        Task<OperationResult> ActualizarAsync(UpdateCarritoDTO entidad);
+        Task<OperationResult> EliminarAsync(RemoveCarritoDTO entidad);
     }
 
 }
