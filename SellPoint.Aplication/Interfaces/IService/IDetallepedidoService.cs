2e7bef4d31c76cde5fa0ac672272d0f1838d20f5
mariaLamarche <mariadelosangeleslamarche@gmail.com>
2025-06-28 21:54:44 -0400

    Add files via upload

--- SellPoint.Aplication/Interfaces/IService/IDetallepedidoService.cs
+++ SellPoint.Aplication/Interfaces/IService/IDetallepedidoService.cs
@@ -1,1 +1,17 @@
 
+using SellPoint.Aplication.Dtos.DetallePedido;
+using SellPoint.Domain.Base;
+
+namespace SellPoint.Aplication.Interfaces.Servicios
+{
+    public interface IDetallepedidoService
+    {
+        Task<OperationResult> ObtenerTodosAsync();
+        Task<OperationResult> ObtenerPorIdAsync(int ObtenerDetallePedidoDTO);
+        Task<OperationResult> AgregarAsync(SaveDetallePedidoDTO saveDetallePedido);
+        Task<OperationResult> ActualizarAsync(UpdateDetallePedidoDTO updateDetallePedido);
+        Task<OperationResult> EliminarAsync(RemoveDetallePedidoDTO RemoveDetallePedido);
+
+
+    }
+}
---
+++ SellPoint.Aplication/Interfaces/IService/IUsuarioService.cs
@@ -0,0 +1,21 @@
+﻿using SellPoint.Aplication.Dtos.Cliente;
+using SellPoint.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SellPoint.Aplication.Interfaces.IService
+{
+    public interface IUsuarioService
+    {
+        Task<OperationResult> ObtenerPorIdAsync(int id);
+        Task<OperationResult> ObtenerTodosAsync();
+        Task<OperationResult> AgregarAsync(SaveClienteDTO saveCliente);
+        Task<OperationResult> ActualizarAsync(UpdateClienteDTO updateCliente);
+        Task<OperationResult> EliminarAsync(RemoveClienteDTO removeCliente);
+
+    }
+}
