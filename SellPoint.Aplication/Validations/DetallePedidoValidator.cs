e61919a200d87507551019ad673f00752ddfe743
mariaLamarche <mariadelosangeleslamarche@gmail.com>
2025-07-02 23:36:36 -0400

    Add files via upload

---
+++ SellPoint.Aplication/Validations/DetallePedidoValidator.cs
@@ -0,0 +1,36 @@
+﻿using SellPoint.Aplication.Dtos.DetallePedido;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SellPoint.Aplication.Validations.DetallePedidoValidator
+{
+    public class DetallePedidoValidator : ValidatorBase<DetallePedidoDTO>
+    {
+        public override bool Validate(DetallePedidoDTO dto, out List<string> errors)
+        {
+            errors = new List<string>();
+
+            if (dto.ProductoId <= 0)
+            {
+                errors.Add("El ID del producto debe ser mayor que 0");
+            }
+
+            if (dto.Cantidad <= 0)
+            {
+                errors.Add("La cantidad debe ser mayor que 0");
+            }
+
+            if (dto.PedidoId <= 0)
+            {
+                errors.Add("El ID del pedido debe ser mayor que 0");
+            }
+
+            return errors.Count == 0; 
+
+        }
+
+    }
+}
---
+++ SellPoint.Aplication/Validations/RemoveClienteValidator.cs
@@ -0,0 +1,22 @@
+﻿using SellPoint.Aplication.Dtos.Cliente;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SellPoint.Aplication.Validations.ClienteValidator
+{
+    public class RemoveClienteValidator: ValidatorBase<RemoveClienteDTO>
+    {
+        public override bool Validate(RemoveClienteDTO dto, out List<string> errors)
+        {
+            errors = new List<string>();
+
+            if (dto.Id <= 0)
+                errors.Add("El ID del cliente debe ser mayor que 0");
+
+            return errors.Count == 0;
+        }
+    }
+}
