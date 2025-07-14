506adaccde91724b89d41606d442d8251a39494a
mariaLamarche <mariadelosangeleslamarche@gmail.com>
2025-07-14 17:41:10 -0400

    Add files via upload

---
+++ SellPoint.IOC1/Dependencies/CuponDependency.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SellPoint.Aplication.Interfaces.IService;
+using SellPoint.Aplication.Interfaces.Repositorios;
+using SellPoint.Aplication.Services.CategoriaService.cs;
+using SellPoint.Aplication.Services.CuponService;
+using SellPoint.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SellPoint.IOC1.Dependencies
+{
+    public static class CuponDependency
+    {
+        public static void AddCuponDependency(this IServiceCollection service)
+        {
+            //service.AddScoped<IDetallePedidoRepository, DetallePedidoRepository>();
+            service.AddScoped<ICuponRepository>(sp =>
+               new CuponRepository(
+        sp.GetRequiredService<IConfiguration>().GetConnectionString("DefaultConnection"),
+        sp.GetRequiredService<ILogger<CuponRepository>>()));
+
+            service.AddScoped<ICuponService, CuponService>();
+        }
+
+    }
+}
