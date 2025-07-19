b58903b539d65e60b43a92fa98b4202999441d84
mariaLamarche <mariadelosangeleslamarche@gmail.com>
2025-07-19 16:17:46 -0400

    Create DbContext.cs

---
+++ SellPoint.Persistence/Context/DbContext.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using SellPoint.Domain.Entities.Orders;
+using SellPoint.Domainn.Entities.Orders;
+using SellPoint.Domainn.Entities.Products;
+using SellPoint.Domainn.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SellPoint.Persistence.Context
+{
+        internal class SellPointDbContext : DbContext
+    {
+        public SellPointDbContext(DbContextOptions<SellPointDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Categoria> Categoria { get; set; }
+            public DbSet<Cupon> Cupon { get; set; }
+            public DbSet<Producto> Producto { get; set; }
+            public DbSet<DetallePedido> DetallePedido { get; set; }
+            public DbSet<Pedido> Pedido { get; set; }
+             public DbSet< Cliente> clientes { get; set; }
+
+    }
+
+}
