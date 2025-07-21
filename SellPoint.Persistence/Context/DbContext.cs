08677e953003bb9028b9427f86e06058765ac34c
mariaLamarche <mariadelosangeleslamarche@gmail.com>
2025-07-20 23:37:15 -0400

    Add files via upload

--- SellPoint.Persistence/Context/DbContext.cs
+++ SellPoint.Persistence/Context/DbContext.cs
@@ -1,29 +1,32 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using SellPoint.Domain.Entities.Orders;
 using SellPoint.Domainn.Entities.Orders;
 using SellPoint.Domainn.Entities.Products;
 using SellPoint.Domainn.Entities.Users;
+using SellPoint.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace SellPoint.Persistence.Context
 {
-        internal class SellPointDbContext : DbContext
-    {
+        public class SellPointDbContext : DbContext
+       {
         public SellPointDbContext(DbContextOptions<SellPointDbContext> options) : base(options)
         {
 
         }
 
         public DbSet<Categoria> Categoria { get; set; }
+            public DbSet<Carrito> Carrito { get; set; }
             public DbSet<Cupon> Cupon { get; set; }
             public DbSet<Producto> Producto { get; set; }
             public DbSet<DetallePedido> DetallePedido { get; set; }
             public DbSet<Pedido> Pedido { get; set; }
-             public DbSet< Cliente> clientes { get; set; }
+             public DbSet< Cliente> Clientes { get; set; }
 
     }
 
