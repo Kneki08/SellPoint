be616bb41c23c27f85ab4023418ce2a136ce0979
mariaLamarche <mariadelosangeleslamarche@gmail.com>
2025-07-20 23:32:21 -0400

    Add files via upload

--- SellPoint.Aplication/Interfaces/IService/IDetallepedidoService.cs
+++ SellPoint.Aplication/Interfaces/IService/IDetallepedidoService.cs
@@ -4,14 +4,14 @@ using SellPoint.Domain.Base;
 
 namespace SellPoint.Aplication.Interfaces.Servicios
 {
-    public interface IDetallepedidoService
+    public interface IDetallepedidoService  
     {
-        Task<OperationResult> ObtenerTodosAsync();
-        Task<OperationResult> ObtenerPorIdAsync(int ObtenerDetallePedidoDTO);
-        Task<OperationResult> AgregarAsync(SaveDetallePedidoDTO saveDetallePedido);
-        Task<OperationResult> ActualizarAsync(UpdateDetallePedidoDTO updateDetallePedido);
-        Task<OperationResult> EliminarAsync(RemoveDetallePedidoDTO RemoveDetallePedido);
-
+        Task<OperationResult> AddAsync(SaveDetallePedidoDTO dto);
+        Task<OperationResult> UpdateAsync(UpdateDetallePedidoDTO dto);
+        Task<OperationResult> DeleteAsync(RemoveDetallePedidoDTO dto);
+        Task<OperationResult> GetByIdAsync(int id);
+        Task<OperationResult> GetAllAsync();
+        Task<OperationResult> SaveChangesAsync();
 
     }
 }
