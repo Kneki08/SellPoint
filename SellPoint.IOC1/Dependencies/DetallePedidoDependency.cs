ced770a1ffae09fe8bb15e6f62f422c00a53d812
mariaLamarche <mariadelosangeleslamarche@gmail.com>
2025-07-20 23:35:54 -0400

    Add files via upload

--- SellPoint.IOC1/Dependencies/DetallePedidoDependency.cs
+++ SellPoint.IOC1/Dependencies/DetallePedidoDependency.cs
@@ -5,6 +5,8 @@ using SellPoint.Aplication.Interfaces.Repositorios;
 using SellPoint.Aplication.Interfaces.Servicios;
 using SellPoint.Aplication.Services.DetallepedidoService;
 using SellPoint.Persistence.Repositories;
+using SellPoint.Domain.Base; 
+using SellPoint.Domain.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,13 +20,9 @@ namespace SellPoint.IOC1.Dependencies
     {
         public static void AddDetalleDependency(this IServiceCollection service)
         {
-            //service.AddScoped<IDetallePedidoRepository, DetallePedidoRepository>();
-            service.AddScoped<IDetallePedidoRepository>(sp =>
-               new DetallePedidoRepository(
-        sp.GetRequiredService<IConfiguration>().GetConnectionString("DefaultConnection"),
-        sp.GetRequiredService<ILogger<DetallePedidoRepository>>()));
+            service.AddScoped<IDetallepedidoRepository, DetallePedidoRepository>();
 
-           service.AddScoped<IDetallepedidoService, DetallepedidoService>();
+            service.AddScoped<IDetallepedidoService, DetallepedidoService>();
         }
 
     }
