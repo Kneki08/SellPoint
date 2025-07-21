894a61b78f8f3906fa69776c4ef4e6cacbd1de3f
mariaLamarche <mariadelosangeleslamarche@gmail.com>
2025-07-20 23:28:23 -0400

    Add files via upload

--- SellPoint.API/Controllers/DetallePedidoController.cs
+++ SellPoint.API/Controllers/DetallePedidoController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Humanizer;
+using Microsoft.AspNetCore.Mvc;
 using SellPoint.Aplication.Dtos.DetallePedido;
 using SellPoint.Aplication.Interfaces.Repositorios;
+using SellPoint.Aplication.Interfaces.Servicios;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,77 +12,52 @@ namespace Sellpoint.api.Controllers
     [ApiController]
     public class DetallePedidoController : ControllerBase
     {
-        private readonly IDetallePedidoRepository _detallePedidoRepository;
-        public DetallePedidoController(IDetallePedidoRepository detallePedidoRepository) 
+        private readonly IDetallepedidoService _detallePedidoService;
+        public DetallePedidoController(IDetallepedidoService detallePedidoService)
         {
-            _detallePedidoRepository = detallePedidoRepository;
+            _detallePedidoService = detallePedidoService;
         }
 
-       
+
         [HttpGet ("ObtenerTodosAsync")]
         public async Task<IActionResult> Get()
         {
-            var result = await _detallePedidoRepository.ObtenerTodosAsync();
-            if (result.IsSuccess)
-            {
-                return Ok(result);
-            }
-
-            else { return BadRequest(result); }
+            var result = await _detallePedidoService.GetAllAsync();
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
         }
 
         // GET api/<DetallePedidoController>/5
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
-            var result = await _detallePedidoRepository.ObtenerPorIdAsync(id);
-            if (result.IsSuccess)
-            {
-                return Ok(result);
-            }
-
-            else { return BadRequest(result); }
+            var result = await _detallePedidoService.GetByIdAsync(id);
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
         }
 
         // POST api/<DetallePedidoController>
         [HttpPost("SaveDetallePedidoDTO")]
-        public async Task<IActionResult> Post([FromBody] SaveDetallePedidoDTO saveDetallePedidoDTO)
+        public async Task<IActionResult> Post([FromBody] SaveDetallePedidoDTO dto)
         {
-            var result = await _detallePedidoRepository.AgregarAsync(saveDetallePedidoDTO);
-            if (result.IsSuccess)
-            {
-                return Ok(result);
-            }
+            // var result = await _detallePedidoRepository.AgregarAsync(saveDetallePedidoDTO);
+            var result = await _detallePedidoService.AddAsync(dto);
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
 
-            else { return BadRequest(result); }
         }
 
         // PUT api/<DetallePedidoController>/5
         [HttpPost("UpdateDetallePedidoDTO")]
-        public async Task<IActionResult> Put([FromBody] UpdateDetallePedidoDTO updateDetallePedidoDTO)
+        public async Task<IActionResult> Put([FromBody] UpdateDetallePedidoDTO dto)
         {
-            var result = await _detallePedidoRepository.ActualizarAsync(updateDetallePedidoDTO);
-            if (result.IsSuccess)
-            {
-                return Ok(result);
-            }
-
-            else { return BadRequest(result); }
-
+            var result = await _detallePedidoService.UpdateAsync(dto);
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
         }
 
         // DELETE api/<DetallePedidoController>/5
         [HttpPost("RemoveDetallePedidoDTO")]
-        public async Task<IActionResult> Put([FromBody] RemoveDetallePedidoDTO removeDetallePedidoDTO)
+        public async Task<IActionResult> Delete([FromBody] RemoveDetallePedidoDTO dto)
         {
-            var result = await _detallePedidoRepository.EliminarAsync(removeDetallePedidoDTO);
-            if (result.IsSuccess)
-            {
-                return Ok(result);
-            }
-
-            else { return BadRequest(result); }
-
+            var result = await _detallePedidoService.DeleteAsync(dto);
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
         }
     }
 }
