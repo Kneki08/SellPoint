7f2b2e6b7adba5e5c1042deb5fcb9031d0fb7a03
mariaLamarche <mariadelosangeleslamarche@gmail.com>
2025-07-25 10:56:03 -0400

    Add files via upload

---
+++ SellPoint.View/Service/DetallePedidoClient/IDetallePedidoApiClient.cs
@@ -0,0 +1,18 @@
+﻿using SellPoint.Aplication.Dtos.DetallePedido;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SellPoint.View.Service.DetallePedidoClient
+{
+    public interface IDetallePedidoApiClient
+    {
+        Task<List<DetallePedidoDTO>> ObtenerTodosAsync();
+        Task<DetallePedidoDTO> ObtenerPorIdAsync(int id);
+        Task<bool> CrearAsync(SaveDetallePedidoDTO dto);
+        Task<bool> ActualizarAsync(UpdateDetallePedidoDTO dto);
+        Task<bool> EliminarAsync(RemoveDetallePedidoDTO dto);
+    }
+}
