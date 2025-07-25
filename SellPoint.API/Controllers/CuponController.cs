900af5b0c148f105e6e3d0ff1e3c0c881e9a6f2b
JeanCarlos-30 <jeancarlosterrero30@gmail.com>
2025-07-25 17:13:34 -0400

    ...

--- SellPoint.API/Controllers/CuponController.cs
+++ SellPoint.API/Controllers/CuponController.cs
@@ -2,53 +2,86 @@
 using SellPoint.Aplication.Dtos.Cupon;
 using SellPoint.Aplication.Interfaces.Repositorios;
 
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace Sellpoint.api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class CuponController : ControllerBase
     {
-        private readonly ICuponRepository _cuponRepository;
-
-        public CuponController(ICuponRepository cuponRepository)
+        private readonly ICuponRepository _CuponRepository;
+        public CuponController(ICuponRepository CuponRepository)
         {
-            _cuponRepository = cuponRepository;
+            _CuponRepository = CuponRepository;
         }
-
+        // GET: api/<PedidoController>
         [HttpGet("ObtenerTodosAsync")]
         public async Task<IActionResult> Get()
         {
-            var result = await _cuponRepository.ObtenerTodosAsync();
-            return result.IsSuccess ? Ok(result) : BadRequest(result);
+            var result = await _CuponRepository.ObtenerTodosAsync();
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+
+            else { return BadRequest(result); }
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
-            var result = await _cuponRepository.ObtenerPorIdAsync(id);
-            return result.IsSuccess ? Ok(result) : BadRequest(result);
+            var result = await _CuponRepository.ObtenerPorIdAsync(id);
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+
+            else { return BadRequest(result); }
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Post([FromBody] SaveCuponDTO dto)
+        // POST api/<PedidoController>
+        [HttpPost("SaveCuponDTO")]
+        public async Task<IActionResult> Post([FromBody] SaveCuponDTO saveCuponDTO)
         {
-            var result = await _cuponRepository.AgregarAsync(dto);
-            return result.IsSuccess ? Ok(result) : BadRequest(result);
+            var result = await _CuponRepository.AgregarAsync(saveCuponDTO);
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+
+            else { return BadRequest(result); }
+
+
         }
 
-        [HttpPut]
-        public async Task<IActionResult> Put([FromBody] UpdateCuponDTO dto)
+        // PUT api/<PedidoController>/5
+        [HttpPost("UpdateCuponDTO")]
+        public async Task<IActionResult> Put([FromBody] UpdateCuponDTO updateCuponDTO)
         {
-            var result = await _cuponRepository.ActualizarAsync(dto);
-            return result.IsSuccess ? Ok(result) : BadRequest(result);
+            var result = await _CuponRepository.ActualizarAsync(updateCuponDTO);
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+
+            else { return BadRequest(result); }
+
+
         }
 
-        [HttpDelete]
-        public async Task<IActionResult> Delete([FromBody] RemoveCuponDTIO dto)
+        // DELETE api/<PedidoController>/5
+        [HttpPost("RemoveCuponDTO")]
+        public async Task<IActionResult> Put([FromBody] RemoveCuponDTIO removeCuponDTO)
         {
-            var result = await _cuponRepository.EliminarAsync(dto);
-            return result.IsSuccess ? Ok(result) : BadRequest(result);
+            var result = await _CuponRepository.EliminarAsync(removeCuponDTO);
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+
+            else { return BadRequest(result); }
+
         }
     }
 }
-
