0bbf429e8d03f18d93b15cce20c17d4cd2f83877
JeanCarlos-30 <jeancarlosterrero30@gmail.com>
2025-07-25 09:50:46 -0400

    ..

--- SellPoint.View/Forms/FormCategoria.Designer.cs
+++ SellPoint.View/Forms/FormCategoria.Designer.cs
@@ -1,22 +1,26 @@
-﻿namespace SellPoint.View
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SellPoint.View
 {
     partial class FormCategoria
     {
         private System.ComponentModel.IContainer components = null;
-        private System.Windows.Forms.DataGridView dgvCategorias;
-        private System.Windows.Forms.TextBox txtId;
-        private System.Windows.Forms.TextBox txtNombre;
-        private System.Windows.Forms.TextBox txtDescripcion;
-        private System.Windows.Forms.Button btnCargar;
-        private System.Windows.Forms.Button btnCrear;
-        private System.Windows.Forms.Button btnActualizar;
-        private System.Windows.Forms.Button btnEliminar;
-        private System.Windows.Forms.Label lblId;
-        private System.Windows.Forms.Label lblNombre;
-        private System.Windows.Forms.Label lblDescripcion;
-        private System.Windows.Forms.GroupBox grpDatos;
-        private System.Windows.Forms.FlowLayoutPanel pnlBotones;
-        private System.Windows.Forms.ToolTip toolTip;
+        private DataGridView dgvCategorias;
+        private TextBox txtId;
+        private TextBox txtNombre;
+        private TextBox txtDescripcion;
+        private Button btnCargar;
+        private Button btnCrear;
+        private Button btnActualizar;
+        private Button btnEliminar;
+        private Label lblId;
+        private Label lblNombre;
+        private Label lblDescripcion;
+        private GroupBox grpDatos;
+        private FlowLayoutPanel pnlBotones;
+        private ToolTip toolTip;
 
         protected override void Dispose(bool disposing)
         {
@@ -46,59 +50,38 @@
             ((System.ComponentModel.ISupportInitialize)(dgvCategorias)).BeginInit();
             SuspendLayout();
 
-            // 
             // grpDatos
-            // 
             grpDatos.Text = "Datos de Categoría";
             grpDatos.Location = new Point(20, 20);
             grpDatos.Size = new Size(440, 130);
             grpDatos.Controls.AddRange(new Control[] { lblId, txtId, lblNombre, txtNombre, lblDescripcion, txtDescripcion });
 
-            // 
             // lblId
-            // 
             lblId.Location = new Point(20, 25);
             lblId.Size = new Size(60, 15);
             lblId.Text = "ID:";
-
             txtId.Location = new Point(100, 22);
             txtId.Size = new Size(300, 23);
 
-            // 
             // lblNombre
-            // 
             lblNombre.Location = new Point(20, 55);
             lblNombre.Size = new Size(60, 15);
             lblNombre.Text = "Nombre:";
-
             txtNombre.Location = new Point(100, 52);
             txtNombre.Size = new Size(300, 23);
 
-            // 
             // lblDescripcion
-            // 
             lblDescripcion.Location = new Point(20, 85);
             lblDescripcion.Size = new Size(75, 15);
             lblDescripcion.Text = "Descripción:";
-
             txtDescripcion.Location = new Point(100, 82);
             txtDescripcion.Size = new Size(300, 23);
 
-            // 
             // pnlBotones
-            // 
             pnlBotones.Location = new Point(480, 20);
             pnlBotones.Size = new Size(140, 130);
             pnlBotones.FlowDirection = FlowDirection.TopDown;
             pnlBotones.Controls.AddRange(new Control[] { btnCargar, btnCrear, btnActualizar, btnEliminar });
-
-            // Configuración de botones con ToolTip
-            void ConfigurarBoton(Button btn, string texto, string tooltip)
-            {
-                btn.Size = new Size(120, 25);
-                btn.Text = texto;
-                toolTip.SetToolTip(btn, tooltip);
-            }
 
             ConfigurarBoton(btnCargar, "Cargar", "Cargar todas las categorías");
             ConfigurarBoton(btnCrear, "Crear", "Crear nueva categoría");
@@ -110,15 +93,11 @@
             btnActualizar.Click += btnActualizar_Click;
             btnEliminar.Click += btnEliminar_Click;
 
-            // 
             // dgvCategorias
-            // 
             dgvCategorias.Location = new Point(20, 170);
             dgvCategorias.Size = new Size(600, 200);
 
-            // 
             // FormCategoria
-            // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(650, 400);
@@ -129,6 +108,14 @@
 
             ((System.ComponentModel.ISupportInitialize)(dgvCategorias)).EndInit();
             ResumeLayout(false);
+        }
+
+        // ✅ Método fuera de InitializeComponent para evitar error en el diseñador
+        private void ConfigurarBoton(Button btn, string texto, string tooltip)
+        {
+            btn.Size = new Size(120, 25);
+            btn.Text = texto;
+            toolTip.SetToolTip(btn, tooltip);
         }
     }
 }
