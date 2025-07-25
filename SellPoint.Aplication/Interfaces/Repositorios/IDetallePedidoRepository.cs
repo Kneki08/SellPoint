a7223692699c055593d6214004d6f402892ed406
mariaLamarche <mariadelosangeleslamarche@gmail.com>
2025-07-25 11:06:30 -0400

    Update IDetallePedidoRepository.cs

--- SellPoint.Aplication/Interfaces/Repositorios/IDetallePedidoRepository.cs
+++ SellPoint.Aplication/Interfaces/Repositorios/IDetallePedidoRepository.cs
@@ -7,12 +7,12 @@ namespace SellPoint.Aplication.Interfaces.Repositorios
 {
     public interface IDetallepedidoRepository
     {
-        Task<OperationResult> GetByIdAsync(int id);
-        Task<OperationResult> GetAllAsync(Expression<Func<DetallePedido, bool>>? filter = null);
-        Task<OperationResult> AddAsync(DetallePedido entity);
-        Task<OperationResult> UpdateAsync(DetallePedido entity);
-        Task<OperationResult> DeleteAsync(int id);
-        Task<OperationResult> SaveChangesAsync();
+      Task<OperationResult> GetByIdAsync(int id);
+      Task<List<DetallePedidoDTO>> GetAllAsync();
+      Task<OperationResult> AddAsync(DetallePedido entity);
+      Task<OperationResult> UpdateAsync(DetallePedido entity);
+      Task<OperationResult> DeleteAsync(int id);
+      Task<OperationResult> SaveChangesAsync();
     }
 
 }
