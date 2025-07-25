beed357633c9128f9cff243973d36752828d3969
mariaLamarche <mariadelosangeleslamarche@gmail.com>
2025-07-25 11:04:36 -0400

    Update UpdateDetallePedidoDTO.cs

--- SellPoint.Aplication/Dtos/DetallePedido/UpdateDetallePedidoDTO.cs
+++ SellPoint.Aplication/Dtos/DetallePedido/UpdateDetallePedidoDTO.cs
@@ -7,7 +7,7 @@ namespace SellPoint.Aplication.Dtos.DetallePedido
         public int Id { get; init; }
         public int Cantidad { get; init; }
         public decimal PrecioUnitario { get; init; }
-        public decimal Subtotal { get; init; }
+       
         public int PedidoId { get; set; }
         public int ProductoId { get; set; }
     }
