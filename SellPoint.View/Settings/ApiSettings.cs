4029dc107151c665e58d587ba7783e3b089b7357
mariaLamarche <mariadelosangeleslamarche@gmail.com>
2025-07-31 20:59:14 -0400

    Create ApiSettings.cs

---
+++ SellPoint.View/Settings/ApiSettings.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SellPoint.View.Settings
+{
+    public class ApiSettings
+    {
+        public string BaseUrl { get; set; }
+        public int TimeoutSeconds { get; set; }
+    }
+}
