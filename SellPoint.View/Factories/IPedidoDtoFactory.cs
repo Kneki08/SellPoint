19e897b80ca1b6a67a6fff31604dc9fc26ae2f1a
Morenaih <soyhfm10@gmail.com>
2025-08-03 22:42:22 -0400

    Sea modificó la carpeta factories para agregar la interfaz IpedidoDtoFactory

---
+++ SellPoint.View/Factories/IPedidoDtoFactory.cs
@@ -0,0 +1,11 @@
+﻿using SellPoint.View.Models.Pedido;
+using SellPoint.View.Validations;
+
+namespace SellPoint.View.Factories
+{
+    public interface IPedidoDtoFactory
+    {
+        SavePedidoDTO CrearSaveDTO(PedidoCamposResult campos, string metodoPago, string referenciaPago, string estado, string notas);
+        UpdatePedidoDTO CrearUpdateDTO(PedidoCamposResult campos, PedidoDTO selected, string metodoPago, string referenciaPago, string estado, string notas);
+    }
+}
--- SellPoint.View/Factories/PedidoDtoFactory.cs
+++ SellPoint.View/Factories/PedidoDtoFactory.cs
@@ -1,45 +1,48 @@
 ﻿using SellPoint.View.Models.Pedido;
 using SellPoint.View.Validations;
 
-public static class PedidoDtoFactory
+namespace SellPoint.View.Factories
 {
-    public static SavePedidoDTO CrearSaveDTO(PedidoCamposResult campos, string metodoPago, string referenciaPago, string estado, string notas)
+    public class PedidoDtoFactory : IPedidoDtoFactory
     {
-        return new SavePedidoDTO
+        public SavePedidoDTO CrearSaveDTO(PedidoCamposResult campos, string metodoPago, string referenciaPago, string estado, string notas)
         {
-            IdUsuario = campos.IdUsuario,
-            IdDireccionEnvio = campos.IdDireccion,
-            MetodoPago = metodoPago,
-            ReferenciaPago = referenciaPago,
-            Subtotal = campos.Subtotal,
-            Descuento = campos.Descuento,
-            CostoEnvio = campos.CostoEnvio,
-            Total = campos.Total,
-            Estado = estado,
-            FechaPedido = DateTime.Now,
-            CuponId = null,
-            Notas = notas
-        };
-    }
+            return new SavePedidoDTO
+            {
+                IdUsuario = campos.IdUsuario,
+                IdDireccionEnvio = campos.IdDireccion,
+                MetodoPago = metodoPago,
+                ReferenciaPago = referenciaPago,
+                Subtotal = campos.Subtotal,
+                Descuento = campos.Descuento,
+                CostoEnvio = campos.CostoEnvio,
+                Total = campos.Total,
+                Estado = estado,
+                FechaPedido = DateTime.Now,
+                CuponId = null,
+                Notas = notas
+            };
+        }
 
-    public static UpdatePedidoDTO CrearUpdateDTO(PedidoCamposResult campos, PedidoDTO selected, string metodoPago, string referenciaPago, string estado, string notas)
-    {
-        return new UpdatePedidoDTO
+        public UpdatePedidoDTO CrearUpdateDTO(PedidoCamposResult campos, PedidoDTO selected, string metodoPago, string referenciaPago, string estado, string notas)
         {
-            Id = selected.Id,
-            IdUsuario = campos.IdUsuario,
-            IdDireccionEnvio = campos.IdDireccion,
-            MetodoPago = metodoPago,
-            ReferenciaPago = referenciaPago,
-            Subtotal = campos.Subtotal,
-            Descuento = campos.Descuento,
-            CostoEnvio = campos.CostoEnvio,
-            Total = campos.Total,
-            Estado = estado,
-            FechaPedido = selected.FechaPedido,
-            FechaActualizacion = DateTime.Now,
-            CuponId = null,
-            Notas = notas
-        };
+            return new UpdatePedidoDTO
+            {
+                Id = selected.Id,
+                IdUsuario = campos.IdUsuario,
+                IdDireccionEnvio = campos.IdDireccion,
+                MetodoPago = metodoPago,
+                ReferenciaPago = referenciaPago,
+                Subtotal = campos.Subtotal,
+                Descuento = campos.Descuento,
+                CostoEnvio = campos.CostoEnvio,
+                Total = campos.Total,
+                Estado = estado,
+                FechaPedido = selected.FechaPedido,
+                FechaActualizacion = DateTime.Now,
+                CuponId = null,
+                Notas = notas
+            };
+        }
     }
 }
